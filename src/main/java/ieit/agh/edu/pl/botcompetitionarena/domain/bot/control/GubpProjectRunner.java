0572d36f2b15a400f5908550d3c38d5b9710ad19
Kinga <k.wierchomska@gmail.com>
2021-04-27 19:15:44 +0200

    DRA-51_2 test

--- src/main/java/ieit/agh/edu/pl/botcompetitionarena/domain/bot/control/GubpProjectRunner.java
+++ src/main/java/ieit/agh/edu/pl/botcompetitionarena/domain/bot/control/GubpProjectRunner.java
@@ -20,6 +20,7 @@ public class GubpProjectRunner {
         // TODO: install requirements.txt automatically, create env
         String envPath = "C:\\Users\\kwier\\Desktop\\studia\\Semestr VI\\GUPB\\venv\\Scripts\\python"; // TODO
         ProcessBuilder pb = new ProcessBuilder("python", "-m", "pip", "install", "-r", "requirements.txt");
+        ProcessBuilder gupbProcessBuilder = new ProcessBuilder("python", "-m", "gupb");
 
         String controllerRelativePath = "GUPB-master/gupb/controller"; //TODO
         String configRelativePath = "GUPB-master/gupb/default_config.py"; //TODO
@@ -33,6 +34,7 @@ public class GubpProjectRunner {
         File workingFolder = new File(folder.toString());
 
         pb.directory(workingFolder);
+        gupbProcessBuilder.directory(workingFolder);
         Process proc = pb.start();
         BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
         BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
@@ -46,8 +48,21 @@ public class GubpProjectRunner {
             System.out.println(s);
             results.add(s);
         }
-        System.out.println(proc.info().toString());
-        return results;
+        System.out.println(proc.isAlive());
+        Process gupbProcess = gupbProcessBuilder.start();
+        BufferedReader stdInput2 = new BufferedReader(new InputStreamReader(gupbProcess.getInputStream()));
+        BufferedReader stdError2 = new BufferedReader(new InputStreamReader(gupbProcess.getErrorStream()));
+        System.out.println("Here is the standard error of the command (if any) - information about queue progress:\n");
+        List<String> results2 = new ArrayList<>();
+        String s2;
+        while ((s2 = stdError2.readLine()) != null)
+            System.out.println(s2);
+        System.out.println("Here is the standard output of the command - information about bots placement:\n");
+        while ((s2 = stdInput2.readLine()) != null) {
+            System.out.println(s2);
+            results.add(s2);
+        }
+        return results2;
     }
 
 }
