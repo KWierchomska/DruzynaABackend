c0a20cebf34c54ba14232088c06ea4221e331dda
Oliwia Masiarek <omoliweeek@gmail.com>
2021-04-28 18:01:44 +0200

    Fix id in bot_queue_assignment table

--- src/main/java/ieit/agh/edu/pl/botcompetitionarena/domain/botqueueassignment/entity/BotQueueAssignmentEntity.java
+++ src/main/java/ieit/agh/edu/pl/botcompetitionarena/domain/botqueueassignment/entity/BotQueueAssignmentEntity.java
@@ -15,7 +15,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 public class BotQueueAssignmentEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private Integer place;
 
